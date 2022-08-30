9a67da7fbe7952a6ca97b575c9b157d63cf8b9e6
Jordan Freundlich <75767764+Jfreundlich01@users.noreply.github.com>
2022-08-30 17:09:58 -0400

    optimizer works and create an xlsx file with lineups

--- optimizer.py
+++ optimizer.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import openpyxl
 from pulp import *
 import re
+
+wb = openpyxl.Workbook()
+ws = wb.active
 
 players = pd.read_csv(r"/Users/jordan/Desktop/J-G/seed.csv", usecols=['Name','Position','Salary','ProjPts'])
 
@@ -10,6 +14,8 @@ availables = availables.reset_index()
 salaries = {}
 points = {}
 
+
+
 for pos in availables.Position.unique():
     available_pos = availables[availables.Position == pos]
     salary = list(available_pos[['Name','Salary']].set_index('Name').to_dict().values())[0]
@@ -18,8 +24,8 @@ for pos in availables.Position.unique():
     salaries[pos] = salary
     points[pos] = point
 
-print(salaries)
-print(points)
+# print(salaries)
+# print(points)
 
 # RB in flex
 pos_num_available = {
@@ -29,5 +35,42 @@ pos_num_available = {
     "TE": 1,
     "DST":1
 }
+
+salary_cap = 50000
+
+
+for lineup in range(1,51):
+    _vars = {k: LpVariable.dict(k, v, cat='Binary') for k, v in points.items()}
+
+    prob = LpProblem("Fantasy", LpMaximize)
+    rewards = []
+    costs = []
+    position_constraints = []
+
+    for k, v in _vars.items():
+        costs += lpSum([salaries[k][i] * _vars[k][i] for i in v])
+        rewards += lpSum([points[k][i] * _vars[k][i] for i in v])
+        prob += lpSum([_vars[k][i] for i in v]) == pos_num_available[k]
+
+    prob += lpSum(rewards)
+    prob += lpSum(costs) <= salary_cap
+    if not lineup == 1:
+        prob += (lpSum(rewards) <= total_score-0.01)
+    prob.solve()
+
+    score= str(prob.objective)
+    constraints = [str(const) for const in prob.constraints.values()]
+    colnum = 1
+
+    for v in prob.variables():
+        score = score.replace(v.name, str(v.varValue))
+        if v.varValue !=0:
+            ws.cell(row=lineup, column=colnum).value = v.name
+            colnum +=1
+    total_score = eval(score)
+    ws.cell(row=lineup, column=colnum).value = total_score
+    print(lineup, total_score)
+
+wb.save(r"playerlists/1.xlsx")
 
 
