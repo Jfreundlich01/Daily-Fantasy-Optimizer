0a231bb171c2382480dac2d6b1a9994fe60a562a
Jordan Freundlich <75767764+Jfreundlich01@users.noreply.github.com>
2022-09-01 16:03:10 -0400

    js optimizer returns a better formatted object

--- optimizer.js
+++ optimizer.js
@@ -4,7 +4,7 @@ let solver = require("javascript-lp-solver"),
     "optimize": "projpts",
     "opType": "max",
     "constraints": {
-        cost: {"max": 23000},
+        cost: {"max": 21500},
         "QB" : {"max": 1,"min":1},
         "WR" : {"max": 3,"min":3},
         "Joe Flacco" : {"max": 1},
@@ -16,43 +16,43 @@ let solver = require("javascript-lp-solver"),
         "Brandin Cooks" : {"max": 1},
     },
     "variables": {
-        "Joe Flacco": {
+        "Joe Flacco QB": {
             "Joe Flacco": 1,
             "projpts": 34.05,
             cost: 4800,
             "QB" : 1
         },
-        "Jameis Winston": {
+        "Jameis Winston QB": {
             "Jameis Winston": 1,
             "projpts": 26.8,
             cost: 5300,
             "QB" : 1
         },
-        "Daniel Jones": {
+        "Daniel Jones QB": {
             "Daniel Jones": 1,
             "projpts": 34,
             cost: 5000,
             "QB" : 1
         },
-        "Davante Adams": {
+        "Davante Adams WR": {
             "Davante Adams": 1,
             "projpts": 18.56,
             cost: 8100,
             "WR" : 1
         },
-        "Deonte Harty": {
+        "Deonte Harty WR": {
             "Deonte Harty": 1,
             "projpts": 6.83,
             cost: 3000,
             "WR" : 1
         },
-        "Michael Pittman Jr.": {
+        "Michael Pittman Jr. WR": {
             "Michael Pittman Jr.": 1,
             "projpts": 11.08,
             cost: 5500,
             "WR" : 1
         },
-        "Brandin Cooks": {
+        "Brandin Cooks WR": {
             "Brandin Cooks": 1,
             "projpts": 11.24,
             cost: 6300,
@@ -61,14 +61,42 @@ let solver = require("javascript-lp-solver"),
         
     },
     "ints": {
-        "Joe Flacco": 1,
-         "Jameis Winston": 1,
-         "Daniel Jones": 1,
-         "Davante Adams": 1,
-         "Michael Pittman": 1,
-         "Deonte Harty": 1,
-         "Brandin Cooks": 1,
+         "Joe Flacco QB": 1,
+         "Jameis Winston QB": 1,
+         "Daniel Jones QB": 1,
+         "Davante Adams WR": 1,
+         "Michael Pittman WR": 1,
+         "Deonte Harty WR": 1,
+         "Brandin Cooks WR": 1,
     }
 }
 results = solver.Solve(model);
-console.log(results);
+
+// console.log(results);
+
+function getObjKey(obj, value) {
+    return Object.keys(obj).filter(key => obj[key] === value)
+}
+
+const arr = getObjKey(results,1)
+
+team = {
+    fptsprpj : results.result,
+    QB:"",
+    WRs: [],
+}
+
+for (const item of arr){
+    itemarr = item.split(' ')
+    if(itemarr.includes("QB")){
+        itemarr.pop()
+        string = itemarr.join(" ")
+        team.QB = string
+    } else if(itemarr.includes("WR")){
+        itemarr.pop()
+        string = itemarr.join(" ")
+        team.WRs.push(string)
+    }
+}
+
+console.log(team)
