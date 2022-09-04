536912150f12a1d7f16db013cf546debc04da015
Jordan Freundlich <75767764+Jfreundlich01@users.noreply.github.com>
2022-09-04 18:33:17 -0400

    solver is running when GET Route hits

--- controllers/lineups.js
+++ controllers/lineups.js
@@ -9,38 +9,51 @@ const Players = require("../models/players.js");
 /////////////////////////////////////////
 const router = express.Router();
 
-router.get("/", (req,res) => {
-    Players.find()
-    .then((players)=>{
-        console.log(players)
-        let constraints = {
-            cost: {"max":50000},
-            "QB": {"max":1,"min":1},
-            "RB": {"max":3,"min":3},
-            "WR": {"max":3,"min":3},
-            "TE": {"max":1,"min":1},
-            "DEF": {"max":1, "min":1},
-        }
-        let variables = {}
-        for(const player of players){
-            constraints[player.Name] = {"max":1}
-            variables[player.Name + " " + player.Position] = {
-            }
-            variables[player.Name + " " + player.Position][player.Name] = 1
-            variables[player.Name + " " + player.Position].projpts = player.ProjPts
-            variables[player.Name + " " + player.Position].cost = player.Salary
-            variables[player.Name + " " + player.Position][player.Position] = 1
-        }
+router.get("/", (req, res) => {
+  Players.find()
+    .then((players) => {
+      console.log(players);
+      let constraints = {
+        cost: { max: 50000 },
+        "QB": { max: 1, min: 1 },
+        "RB": { max: 3, min: 3 },
+        "WR": { max: 3, min: 3 },
+        "TE": { max: 1, min: 1 },
+        "DST": { max: 1, min: 1 },
+      };
+      let variables = {};
+      let ints = {};
+      for (const player of players) {
+        constraints[player.Name] = { max: 1 };
+        variables[player.Name + " " + player.Position] = {};
+        variables[player.Name + " " + player.Position][player.Name] = 1;
+        variables[player.Name + " " + player.Position].projpts = player.ProjPts;
+        variables[player.Name + " " + player.Position].cost = player.Salary;
+        variables[player.Name + " " + player.Position][player.Position] = 1;
+        ints[player.Name + " " + player.Position] = 1;
+      }
 
-        res.send(variables)
-    })
-    .catch((error) =>{
-        console.log(error)
-        console.log('this is an error')
-        res.json(error)
-    })
-})
+      let solver = require("javascript-lp-solver"),
+        results,
+        model = {
+          "optimize": "projpts",
+          "opType": "max",
+          "constraints": constraints,
+          "variables": variables,
+          "ints": ints,
+        };
+      results = solver.Solve(model);
 
+      console.log(model.constraints);
+
+      res.send(results);
+    })
+    .catch((error) => {
+      console.log(error);
+      console.log("this is an error");
+      res.json(error);
+    });
+});
 
 //////////////////////////////////////////
 // Export the Router
--- optimizer.js
+++ optimizer.js
@@ -5,60 +5,59 @@ let solver = require("javascript-lp-solver"),
     "opType": "max",
     "constraints": {
         cost: {"max": 21500},
-        "QB" : {"max": 1,"min":1},
-        "WR" : {"max": 3,"min":3},
+        QB : {"max": 1,"min":1},
+        WR : {"max": 3,"min":3},
         "Joe Flacco" : {"max": 1},
         "Jameis Winston" : {"max": 1},
         "Daniel Jones" : {"max": 1},
         "Davante Adams" : {"max": 1},
         "Deonte Harty" : {"max": 1},
         "Michael Pittman Jr." : {"max": 1},
-        "Brandin Cooks" : {"max": 1},
+        "Brandin Cooks" : {"max": 1}
     },
     "variables": {
         "Joe Flacco QB": {
             "Joe Flacco": 1,
-            "projpts": 34.05,
+            projpts: 34.05,
             cost: 4800,
             "QB" : 1
         },
         "Jameis Winston QB": {
             "Jameis Winston": 1,
-            "projpts": 26.8,
+            projpts: 26.8,
             cost: 5300,
             "QB" : 1
         },
         "Daniel Jones QB": {
             "Daniel Jones": 1,
-            "projpts": 34,
+            projpts: 34,
             cost: 5000,
             "QB" : 1
         },
         "Davante Adams WR": {
             "Davante Adams": 1,
-            "projpts": 18.56,
+            projpts: 18.56,
             cost: 8100,
             "WR" : 1
         },
         "Deonte Harty WR": {
             "Deonte Harty": 1,
-            "projpts": 6.83,
+            projpts: 6.83,
             cost: 3000,
             "WR" : 1
         },
         "Michael Pittman Jr. WR": {
             "Michael Pittman Jr.": 1,
-            "projpts": 11.08,
+            projpts: 11.08,
             cost: 5500,
             "WR" : 1
         },
         "Brandin Cooks WR": {
             "Brandin Cooks": 1,
-            "projpts": 11.24,
+            projpts: 11.24,
             cost: 6300,
             "WR" : 1
-        },
-        
+        }  
     },
     "ints": {
          "Joe Flacco QB": 1,
@@ -72,7 +71,7 @@ let solver = require("javascript-lp-solver"),
 }
 results = solver.Solve(model);
 
-// console.log(results);
+console.log(results);
 
 function getObjKey(obj, value) {
     return Object.keys(obj).filter(key => obj[key] === value)
